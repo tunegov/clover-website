b5271dc92d0e6ed4b344a68383cc06a26f18e881
Sergey Tunegov <sergeytunegov@Sergeys-MacBook-Air.local>
2018-09-21 22:02:16 +0300

    Added server

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
+const express = require('express')
+const path = require('path')
+const app = express()
 
+app.use(express.static('public'));
 
-var fs = require('fs');
-var path = require('path');
-
-exports.get = function (event, context, callback) {
-  var contents = fs.readFileSync(`build${path.sep}index.html`);
-  var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: { 'content-type': 'text/html' }
-  };
-
-  callback(null, result);
-};
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public.index.html'))
+})
+app.listen(80, () => console.log('Server running on port 80'))
