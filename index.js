248666ebbe8140bb3d37384ed89cee60a6c4d17e
Sergey Tunegov <sergeytunegov@Sergeys-MacBook-Air.local>
2018-09-21 19:54:11 +0300

    Fixed index file

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-'use strict';
+
 
 var fs = require('fs');
 var path = require('path');
 
-exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
+exports.get = function (event, context, callback) {
+  var contents = fs.readFileSync(`build${path.sep}index.html`);
   var result = {
     statusCode: 200,
     body: contents.toString(),
-    headers: {'content-type': 'text/html'}
+    headers: { 'content-type': 'text/html' }
   };
 
   callback(null, result);
