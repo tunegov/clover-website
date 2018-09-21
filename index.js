6b9e0674580a98fe5188e4e06fe348fcf6866cdb
Sergey Tunegov <sergeytunegov@Sergeys-MacBook-Air.local>
2018-09-22 00:19:59 +0300

    Changed ports

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const express = require('express')
-const fs = require('fs')
 const path = require('path')
-const https = require('https')
 const app = express()
 
 app.use(express.static('public'));
-app.use(function (req, res, next) {
-  if (req.secure) {
-    next();
-  } else {
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-});
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public.index.html'))
+  res.sendFile(path.join(__dirname, 'public./ndex.html'))
 })
 
 
-app.listen(80, () => console.log('HTTP Server running on port 80'))
-
-https.createServer({}, app).listen(443, () => console.log('HTTPS Server running on port 80'))
+app.listen(8080, () => console.log('HTTP Server running on port 8080'))
+app.listen(8443, () => console.log('HTTPS Server running on port 8443'))
