10591524f0119227bc6cfee42bc73a3cd9eb2318
Sergey Tunegov <sergeytunegov@Sergeys-MacBook-Air.local>
2018-09-23 17:44:21 +0300

    Added social links

--- src/App.js
+++ src/App.js
@@ -3,16 +3,39 @@ import Lottie from "react-lottie";
 import { loader } from "./loader.js";
 import "./App.css";
 import scooter from "./images/scooter.svg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faInstagram, faFacebookF } from '@fortawesome/free-brands-svg-icons'
+
+
+const socialMediaIcons = [
+  {
+    url: 'https://www.instagram.com/clover_company_/',
+    icon: faInstagram,
+  },
+  {
+    url: 'https://twitter.com/clover_company_',
+    icon: faTwitter,
+  },
+  {
+    url: 'https://www.facebook.com/clover.company.real/',
+    icon: faFacebookF,
+  },
+];
 
 class App extends PureComponent {
   constructor(props) {
     super(props);
     this.state = { isStopped: false, isPaused: true };
   }
+
   componentDidMount() {
     setTimeout(() => {
       this.animation.play()
     }, 2000)
+  }
+
+  _redirectTo(url) {
+    window.open(url, "_blank")
   }
 
   render() {
@@ -33,8 +56,15 @@ class App extends PureComponent {
           ref={ref => this.animation = ref}
         />
         {/* <span className="company-name">CLOVER COMPANY</span> */}
-        <span className="cooming-soon">Be Part of The Future</span>
-        <img src={scooter} alt="scooter" className="scooter" />
+        <h2 className="cooming-soon">Be Part of The Future</h2>
+        <img src={scooter} alt="clover-scooter" className="scooter" />
+        <div className="social-icons">
+          {socialMediaIcons.map((i) => (
+            <div className="social-icon" onClick={this._redirectTo.bind(this, i.url)}>
+              <FontAwesomeIcon icon={i.icon} color={"white"} size={"2x"} fixedWidth={50} />
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
